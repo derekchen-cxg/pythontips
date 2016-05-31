55eb3b3c810c784c76393350d608156750461671
derek <derekchen.cxg@gmail.com>
2016-05-31 13:43:56 +0800

    second version

--- bucongfusuzi.py
+++ bucongfusuzi.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env
+s="45678"
 a=[]
 c=[]
 b=""
-for x in range(1,5):
-    for y in range(1,5):
-        for z in range(1,5):
-            b=str(z)+str(y)+str(x)
+for x in range(len(s)):
+    for y in range(len(s)):
+        for z in range(len(s)):
+            b=str(s[z])+str(s[y])+str(s[x])
             if b[0] != b[1] and b[0] != b[2] and b[1] != b[2]:
                 a.append(b)
             else:
