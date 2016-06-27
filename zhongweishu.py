4fc00c59622229b577ca41794c74a1548a584a98
derek <derekchen.cxg@gmail.com>
2016-06-27 17:22:12 +0800

    mark a litte

---
+++ zhongweishu.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+L=[]
+for i in range(6):
+    L.append(input("Enter your number:"))
+L.sort()
+i=len(L)
+if i%2 == 0:
+    print(i)
+    print(L)
+    print(L[int(i/2)],L[int(i/2-1)])
+else:
+    print(L)
+    print(L[int((i-1)/2)])
+
