a5310cd3ea58d9f442d350ae3e9c0f269943ee1f
derek <derekchen.cxg@gmail.com>
2016-07-18 16:25:42 +0800

    1.文件操作里的,w/r/a/r+区别 2.re模块对文件内容的查找和替换 3.日志模块的功能

---
+++ monitorfiles.py
@@ -0,0 +1,28 @@
+#/usr/bin/env python
+import re
+import logging
+log_filename='monitorgateway_log'
+log_format='%(asctime)s [%(filename)s] %(message)s'
+logging.basicConfig(filename=log_filename,filemode='a',format=log_format,datefmt='%Y-%m-%d %H:%M:%S %p',level=logging.DEBUG)
+f_w=open('1.txt','a')
+f_z=open('1.txt','r+')
+l=['192.168.121.1\n','192.168.122.1\n']
+count_g1=
+count_g2=0
+for line in open('1.txt'):
+    g1=re.findall('192.168.121.1',line)
+    g2=re.findall('192.168.122.1',line)
+    count_g1=count_g1 + len(g1)
+    count_g2=count_g2 + len(g2)
+if count_g1==0 and count_g2==0:
+    f_z.writelines(l)
+    f_z.close()
+    logging.warn('gateway.txt add 192.168.121.1,192.168.122.1')
+elif count_g1==0 and count_g2>0:
+    f_w.write('192.168.121.1\n')
+    f_w.close()
+    logging.warn('gateway.txt add 192.168.121.1')
+elif count_g1>0 and count_g2==0:
+    f_w.write('192.168.122.1\n')
+    f_w.close()
+    logging.warn('gateway.txt add 192.168.122.1')
